cd169fcd5c64b9a2460f6afc9068bcf7c1a84dab
Amar Singara <amarsingara@hotmail.com>
2020-10-15 22:30:29 -0700

    first commit

---
+++ Models/Book.cs
@@ -0,0 +1,13 @@
+
+namespace Assignment1.Models {
+public class Book {
+  public string title { get; set; }
+  public string smallThumbnail { get; set; }
+  public string authors { get; set; }
+  public string publisher { get; set; }
+  public string publishedDate { get; set; }
+  public string description { get; set; }
+  public string ISBN10 { get; set; }
+}
+
+}
